82d377c903888ba03d253354388b7a4555c9a043
Ruslan Beksultanov <ruslan.beksultanov@mbank.kg>
2025-06-30 09:49:14 +0600

    added animation value

--- lib/src/widget/card_swiper_state.dart
+++ lib/src/widget/card_swiper_state.dart
@@ -42,6 +42,7 @@ class _CardSwiperState<T extends Widget> extends State<CardSwiper>
     _newCardAnimationController = AnimationController(
       duration: widget.duration,
       vsync: this,
+      value: 1.0,
     );
 
     _cardAnimation = CardAnimation(
@@ -90,14 +91,10 @@ class _CardSwiperState<T extends Widget> extends State<CardSwiper>
               return Stack(
                 clipBehavior: Clip.none,
                 fit: StackFit.expand,
-                children: List.generate(
-                  numberOfCardsOnScreen(),
-                  (index) {
-                    final realIndex = numberOfCardsOnScreen() - 1 - index;
-                    if (realIndex == 0) return _frontItem(constraints);
-                    return _backItem(constraints, realIndex);
-                  },
-                ),
+                children: List.generate(numberOfCardsOnScreen(), (index) {
+                  if (index == 0) return _frontItem(constraints);
+                  return _backItem(constraints, index);
+                }).reversed.toList(),
               );
             },
           ),
